88b7fbd2eb66599c29082b5cde451f57e874c053
nullptrerror <59756494+nullptrerror@users.noreply.github.com>
2023-08-31 18:53:06 -0400

    Added scenario 4 and configured firefox driver (#10)
    Co-authored-by: Matt <mmatiasn@gmail.com>

--- Selenium_E2E/TestSuitScenarios/Scenario2_GoogleSearch.cs
+++ Selenium_E2E/TestSuitScenarios/Scenario2_GoogleSearch.cs
@@ -2,9 +2,8 @@
 
 namespace Selenium_E2E.TestSuitScenarios
 {
-    [Parallelizable(ParallelScope.Self)]
     [TestFixture]
-    public class Tests : PageTest
+    public partial class Tests : PageTest
     {
         [Test]
         public async Task Scenario2_GoogleSearch()
@@ -18,7 +17,7 @@ namespace Selenium_E2E.TestSuitScenarios
             await Page.GetByText("Florida", new() { Exact = true }).ClickAsync();
 
             // Wait ten seconds
-            await Page.WaitForTimeoutAsync(10000);
+            await Page.WaitForTimeoutAsync(5000);
         }
     }
 }
---
+++ Selenium_E2E/TestSuitScenarios/Scenario4_TicketMaster.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Selenium_E2E.TestSuitScenarios
+{
+    [TestFixture]
+    public partial class Tests : PageTest
+    {
+        [Test]
+        public async Task Scenario4_TicketMaster()
+        {
+            await Page.GotoAsync("https://www.google.com/");
+
+            await Page.GetByLabel("Search", new() { Exact = true }).ClickAsync();
+
+            await Page.GetByLabel("Search", new() { Exact = true }).FillAsync("ticketmaster");
+
+            await Page.GetByLabel("Google Search").First.ClickAsync();
+
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Ticketmaster: Buy Verified Tickets for Concerts, Sports ... Ticketmaster https://www.ticketmaster.com" }).ClickAsync();
+
+            await Page.GetByPlaceholder("Search for artists, venues, and events").ClickAsync();
+
+            await Page.GetByPlaceholder("Search for artists, venues, and events").FillAsync("Tayler Swift");
+
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Search", Exact = true }).ClickAsync();
+
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Find tickets Taylor Swift | The Eras Tour Miami, FL Hard Rock Stadium 10/18/24, 7:00 PM" }).ClickAsync();
+
+        }
+    }
+}
