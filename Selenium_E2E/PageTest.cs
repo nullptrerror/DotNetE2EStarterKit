29eeeac5454382216ea5c540edae7fbbc6ff47d7
Matt <mmatiasn@gmail.com>
2023-09-22 19:25:08 -0400

    Added recording logic to testing.

--- Selenium_E2E/PageTest.cs
+++ Selenium_E2E/PageTest.cs
@@ -2,6 +2,7 @@ using MicrosoftPlaywrightPlaywright = Microsoft.Playwright.Playwright;
 using Microsoft.Playwright;
 using NUnit.Framework;
 using NUnit_E2E.Settings;
+using System.Reflection;
 
 namespace NUnit_E2E;
 
@@ -14,13 +15,28 @@ public class PageTest
     internal IBrowserContext Context { get; set; } = null!;
     internal IPage Page { get; set; } = null!;
     internal TestAppSettings configuration = null!;
+    internal string currentVideoDir {
+        get {
+            // Get the current executing directory
+            string executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new Exception("Could not get executing directory");
+        
+            // Build the video directory path relative to the executing directory
+            string videoDirectory = Path.Combine(executingDirectory, $"videos\\{TestContext.CurrentContext.Test.MethodName}\\");
+        
+            return videoDirectory;
+        }
+    }
 
     [SetUp]
     public async Task BaseSetup()
     {
         Playwright = await MicrosoftPlaywrightPlaywright.CreateAsync();
-        Browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = Headless });
-        Context = await Browser.NewContextAsync(new BrowserNewContextOptions { UserAgent = UserAgent });
+        Browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = Headless });
+        Context = await Browser.NewContextAsync(new BrowserNewContextOptions { 
+            UserAgent = UserAgent,
+            RecordVideoDir = currentVideoDir,
+            RecordVideoSize = new RecordVideoSize { Width = 1280, Height = 720 }
+         });
         Page = await Context.NewPageAsync();
     }
 
@@ -29,6 +45,20 @@ public class PageTest
     {
         await Context.CloseAsync();
         await Browser.CloseAsync();
+        // Rename the video after the context is closed
+        var videoFiles = Directory.GetFiles(currentVideoDir);
+        if (videoFiles.Length > 0)
+        {
+            var videoFilePath = videoFiles[0];
+            var parentDir = Directory.GetParent(videoFilePath)?.Parent?.FullName ?? throw new Exception("Could not get parent directory");
+            var desiredVideoName = Path.Combine(parentDir, $"{DateTime.Now:yyyyMMdd_HHmmss}_{TestContext.CurrentContext.Test.MethodName}.webm");
+            File.Move(videoFilePath, desiredVideoName);
+            // Delete directory if it exists
+            if (Directory.Exists(currentVideoDir))
+            {
+                Directory.Delete(currentVideoDir, true);
+            }
+        }
     }
 
     [OneTimeTearDown]
