c9f270b6f694c4bfcc68b82a6f381df97a094fd7
Matt <mmatiasn@gmail.com>
2023-09-23 18:12:07 -0400

    Cleaning up project

--- NUnit_E2E/TestSuitScenarios/Scenario1_GoogleSearch.cs.cs
+++ NUnit_E2E/TestSuitScenarios/Scenario1_GoogleSearch.cs.cs
@@ -1,24 +1,36 @@
-﻿using NUnit.Framework;
+﻿using NUnit.Allure.Attributes;
+using NUnit.Framework;
+using System.Collections;
 
 namespace NUnit_E2E.TestSuitScenarios
 {
-    [Parallelizable(ParallelScope.Self)]
-    [TestFixture]
     public partial class Tests : PageTest
     {
+        internal static IEnumerable TestCasesSearches
+        {
+            get
+            {
+                yield return new TestCaseData("Texas").Returns("texas - Google Search");
+                yield return new TestCaseData("Florida").Returns("florida - Google Search");
+                yield return new TestCaseData("California").Returns("california - Google Search");
+            }
+        }
+
         [Test]
-        public async Task Scenario1_GoogleSearch()
+        [AllureSubSuite(nameof(Scenario1_GoogleSearch))]
+        [AllureTag(nameof(Scenario1_GoogleSearch))]
+        [TestCaseSource(typeof(Tests), nameof(TestCasesSearches))]
+        public async Task<string> Scenario1_GoogleSearch(string q)
         {
             await Page.GotoAsync("https://www.google.com/");
 
             await Page.GetByLabel("Search", new() { Exact = true }).ClickAsync();
 
-            await Page.GetByLabel("Search", new() { Exact = true }).FillAsync("Texas");
+            await Page.GetByLabel("Search", new() { Exact = true }).FillAsync(q);
 
-            await Page.GetByText("Texas", new() { Exact = true }).ClickAsync();
+            await Page.GetByText(q, new() { Exact = true }).ClickAsync();
 
-            // Wait ten seconds
-            await Page.WaitForTimeoutAsync(10000);
+            return await Page.TitleAsync();
         }
     }
 }
---
+++ NUnit_E2E/TestSuitScenarios/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Allure.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_E2E.TestSuitScenarios
+{
+    [AllureSuite(nameof(Tests))]
+    public partial class Tests : PageTest
+    {
+    }
+}
