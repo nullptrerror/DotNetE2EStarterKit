334ee3f3883b56c2ec4eb12a37141782b8f31731
nullptrerror <59756494+nullptrerror@users.noreply.github.com>
2023-09-27 20:10:07 -0400

    Feature/path changes (#37)
    * docs(README.md): Updating codegen instructions.
    
    * Removed redundant code
    
    * simplified path issues
    
    ---------
    
    Co-authored-by: Matt Miranda Noriega <MMirandaNoriega@pbcgov.org>

--- NUnit_E2E/PageTest.cs
+++ NUnit_E2E/PageTest.cs
@@ -34,32 +34,6 @@ public class PageTest
     internal sealed class PageConsts
     {
         /// <summary>
-        /// Constants for reflection of Page.Video object
-        /// </summary>
-        internal sealed class VideoReflection
-        {
-            /// <summary>
-            /// The name of the _artifactTcs field in Page.Video._artifactTcs
-            /// </summary>
-            internal const string _artifactTcs = nameof(_artifactTcs);
-            /// <summary>
-            /// The name of the Task property Page.Video._artifactTcs.Task
-            /// </summary>
-            internal const string Task = nameof(Task);
-            /// <summary>
-            /// The name of the Result property Page.Video._artifactTcs.Task.Result
-            /// </summary>
-            internal const string Result = nameof(Result);
-            /// <summary>
-            /// The name of the AbsolutePath property Page.Video._artifactTcs.Task.Result.AbsolutePath
-            /// </summary>
-            internal const string AbsolutePath = nameof(AbsolutePath);
-            /// <summary>
-            /// The error message template for when the video absolute path cannot be fetched
-            /// </summary>
-            internal const string ErrorMessageTemplate = "Error fetching video absolute path: {0}";
-        }
-        /// <summary>
         /// Constants for Page object
         /// </summary>
         internal sealed class Browser
@@ -75,7 +49,7 @@ public class PageTest
             /// <summary>
             /// The template for the trace file name
             /// </summary>
-            internal const string TraceFileTypeTemplate = "{0}{1}{2}.zip";
+            internal const string TraceFileTypeTemplate = "{0}{1}{2}.playwright.trace.zip";
             /// <summary>
             /// The error message template for when the page cannot be closed
             /// </summary>
@@ -225,23 +199,23 @@ public class PageTest
     [TearDown]
     public async Task BaseTearDown()
     {
-        var videoPath = _getVideoAbsolutePath();
-
         try
         {
-            if (!Page.IsClosed) await Page.WaitForLoadStateAsync();
+            if (!Page.IsClosed) await Page.WaitForLoadStateAsync(LoadState.NetworkIdle, PageConsts.Browser.TeardownPageWaitForLoadStateOptions);
         }
         catch (Exception ex)
         {
             Console.WriteLine(string.Format(PageConsts.Browser.WaitForLoadStateErrorMessageTemplate, ex.Message));
         }
 
-        // Get filename without extension
-        var traceFileName = string.IsNullOrWhiteSpace(videoPath) ? Guid.NewGuid().ToString("N") : Path.GetFileNameWithoutExtension(videoPath);
+        var absoluteVideoFilePath = Page.Video != null ? await Page.Video.PathAsync() : string.Empty;
+        var commonFileName = Page.Video != null
+                                ? Path.GetFileNameWithoutExtension(absoluteVideoFilePath)
+                                : Guid.NewGuid().ToString("N");
         var absoluteTraceFilePath = string.Format(PageConsts.Browser.TraceFileTypeTemplate,
                                                     AbsoluteAssetsDir,
                                                     Path.DirectorySeparatorChar,
-                                                    traceFileName);
+                                                    commonFileName);
         // Stop tracing and export it into a zip archive.
         await Context.Tracing.StopAsync(new TracingStopOptions()
         {
@@ -252,52 +226,14 @@ public class PageTest
         await Context.CloseAsync();
         await Browser.CloseAsync();
 
-        // Playwright should give us the name and even absolute path of the video file.
-        // I will suggest it or even see if I can implement it myself.
-        // We could call it before or after close so it would be a cleaner solution.
-        // var videoPath = Page.Video.Path;
-
         // Add the video to the allure report, must be done after the context is closed, otherwise the video file will be locked.
-        if (videoPath != null) AllureLifecycle.Instance.AddAttachment(TestContext.CurrentContext.Test.MethodName ?? string.Empty, PageConsts.Browser.VideoType, videoPath);
+        if (Page.Video != null) AllureLifecycle.Instance.AddAttachment(string.Concat(PageConsts.Browser.VideoType, " - ", TestContext.CurrentContext.Test.MethodName),
+                                                                        PageConsts.Browser.VideoType,
+                                                                        absoluteVideoFilePath);
         // Add the tracing to the allure report
-        AllureLifecycle.Instance.AddAttachment(TestContext.CurrentContext.Test.MethodName ?? string.Empty, PageConsts.Browser.TracingType, absoluteTraceFilePath);
-    }
-    /// <summary>
-    /// Get video file path with name from the page object before we close the context, otherwise the video file name won't be in the Page.Video object
-    /// </summary>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
-    private string? _getVideoAbsolutePath()
-    {
-        // Ensure we have a page object
-        if (Page == null) throw new ArgumentNullException(nameof(Page));
-
-        try
-        {
-            // Get the non-public _artifactTcs field from the Video object
-            var _artifactTcsField = Page?.Video?.GetType().GetField(PageConsts.VideoReflection._artifactTcs, BindingFlags.NonPublic | BindingFlags.Instance);
-            var _artifactTcsValue = _artifactTcsField?.GetValue(Page?.Video);
-
-            // Get the Task property and its value
-            var taskProperty = _artifactTcsValue?.GetType().GetProperty(PageConsts.VideoReflection.Task);
-            var taskValue = taskProperty?.GetValue(_artifactTcsValue) as Task;
-
-            // Get the Result property and its value
-            var taskResultProperty = taskValue?.GetType().GetProperty(PageConsts.VideoReflection.Result);
-            var artifactResult = taskResultProperty?.GetValue(taskValue);
-
-            // Get the AbsolutePath property and its value
-            var absolutePathProperty = artifactResult?.GetType().GetProperty(PageConsts.VideoReflection.AbsolutePath, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            var absolutePathValue = absolutePathProperty?.GetValue(artifactResult) as string;
-
-            return absolutePathValue;
-        }
-        catch (Exception ex)
-        {
-            // Handle or log the exception as appropriate
-            Console.WriteLine(string.Format(PageConsts.VideoReflection.ErrorMessageTemplate, ex.Message));
-            return null;  // or rethrow, based on your requirements
-        }
+        AllureLifecycle.Instance.AddAttachment(string.Concat(PageConsts.Browser.TracingType, " - ", TestContext.CurrentContext.Test.MethodName),
+                                                PageConsts.Browser.TracingType,
+                                                absoluteTraceFilePath);
     }
     /// <summary>
     /// Tear down the test
