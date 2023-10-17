6d0beceae400c53f0cadbccf855538847a90fa0f
Matt <mmatiasn@gmail.com>
2023-10-16 20:40:40 -0400

    deleting old project

--- NUnit_E2E/Settings/TestAppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NUnit_E2E.Settings
-{
-    public sealed class TestAppSettings
-    {
-        public string StartingChromeUrl { get; set; } = null!;
-        public Users Users { get; set; } = null!;
-    }
-}
--- NUnit_E2E/Settings/Users.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NUnit_E2E.Settings
-{
-    public sealed class Users
-    {
-        public Admin Admin { get; set; } = null!;
-    }
-}
